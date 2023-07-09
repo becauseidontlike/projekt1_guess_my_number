5345fa906af81538b9dc9854b9b96357ab0ee80f
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-07-09 19:22:15 +0200

    event listeners for the game

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct number!';
+// document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 13;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 13;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Check your lucky number!';
+  }
+});
