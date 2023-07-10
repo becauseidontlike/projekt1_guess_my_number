d34250138da60f1409c1e5e6fd74ed1e63d248e7
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-07-10 22:24:26 +0200

    game logic in js

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 
 // console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.message').textContent = 'Correct number!';
+// document.querySelector('.message').textContent = 'Correct secretNumber!';
 
-// document.querySelector('.number').textContent = 13;
+// document.querySelector('.secretNumber').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 
 // console.log(document.querySelector('.guess').value);
 // document.querySelector('.guess').value = 13;
 // console.log(document.querySelector('.guess').value);
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,5 +23,15 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Check your lucky number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
