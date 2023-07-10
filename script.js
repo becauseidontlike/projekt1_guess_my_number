6cea6491734a819ba8cf3c619b0ea29b4a39a194
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-07-10 22:41:57 +0200

    css properties changed for different results

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 //console.log(secretNumber);
-document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
 
@@ -25,6 +24,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Check your lucky number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -33,6 +35,7 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#f31818';
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
@@ -42,6 +45,7 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#f31818';
     }
   }
 });
